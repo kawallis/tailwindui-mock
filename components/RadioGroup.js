dffcf22881bed9e91d77f5cf37fd90b54390ed5a
Christopher Wallace <christopherwallace@Christophers-MacBook-Pro.local>
2021-12-09 19:18:30 -0800

    update

--- components/RadioGroup.js
+++ components/RadioGroup.js
@@ -7,7 +7,6 @@ export const RadioGroup = ({ notificationMethods, name }) => {
             id={notificationMethod.id}
             name={name}
             type="radio"
-            checked={notificationMethod.enabled}
             defaultChecked={notificationMethod.enabled}
             className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300"
           />
